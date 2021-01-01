eb7102fdd53072019793a984ef58c002d139e08a
Prasang Gupta <prasang.tix@gmail.com>
2021-01-01 16:08:15 +0530

    organised leetcode monthly challenges

---
+++ leetcode/monthly-leetcoding-challenge/2021-01/day-01-CheckArrayFormationThroughConcatenation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        for (int i = 0; i < arr.size(); ++i){
+            int flag = 0;
+            for (int j = 0; j < pieces.size(); ++j){
+                if (arr[i] == pieces[j][0]){
+                    flag = 1;
+                    for (int k = 0; k < pieces[j].size(); ++k){
+                        if (arr[i] != pieces[j][k]){
+                            return false;
+                        }
+                        ++i;
+                    }
+                    --i;
+                    break;
+                }
+            }
+            if (flag == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
